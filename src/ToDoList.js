28932b880a771fd7e99afc41d01619ab76b0f937
Krystian Pietroń <krystianpietr@gmail.com>
2018-12-04 11:15:06 +0100

    add style for div todo list

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { addToDo } from './Store/ToDos';
+import { addToDo, toggleTodo } from './Store/ToDos';
 
 
 const mapStateToProps = state => ({
     _todos: state.todos.allTodos,
-    _visibleTodos: state.todos.visibleTodos
+    _visibleTodos: state.todos.visibleTodos,
+
 })
 const mapDispatchToProps = dispatch => ({
-    _addToDo: text => dispatch(addToDo(text))
+    _addToDo: text => dispatch(addToDo(text)),
+    _toogleTodo: index => dispatch(toggleTodo(index))
 })
 
 
@@ -22,13 +24,19 @@ class ToDoList extends React.Component {
         console.log('want to save todo: ', this.state.value)
         this.props._addToDo(this.state.value)
     }
+    handleTodoClick = (index) => {
+        this.props._toogleTodo(index)
+    }
     render() {
         return (
             <div>
                 <input onChange={this.handleInputChange} />
                 <button onClick={this.handleButtonClick}>Add ToDo</button>
-                {this.props._todos.map(todo =>
-                    <div key={todo.text}>
+                {this.props._todos.map((todo, index) =>
+                    <div key={todo.text}
+                        style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
+                        onClick={() => this.handleTodoClick(index)}
+                    >
                         {todo.text}
                     </div>
                 )}
