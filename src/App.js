90081436f9aeadd2a205d8f81f3df5a899c61033
Krystian Pietroń <krystianpietr@gmail.com>
2018-12-04 10:14:19 +0100

    add todos handleInputChange and handleButtonClick

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+
 import ToDoList from './ToDoList'
+
+const mapStateToProps = state => ({
+  todos: state.todos.allTodos,
+  visibleTodos: state.todos.visibleTodos
+})
 
 class App extends Component {
   render() {
     return (
-      <ToDoList todos={[
-        { text: 'First todo from App' },
-        { text: 'Second todo from App' }
-      ]} />
+      <ToDoList todos={this.props.todos}
+        visibleTodos={this.props.visibleTodos} />
     );
   }
 }
 
-export default App;
+export default connect(mapStateToProps)(App)
--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,12 +1,30 @@
 import React from 'react'
 
 class ToDoList extends React.Component {
+   state  = {}
+
+   handleInputChange = event => {
+       this.state({value: event.target.value})
+   }
+
+handleButtonClick = () => {
+    console.log('want to save todo: ',this.state.value)
+}
+   
+   
     render() {
         return (
             <div>
+                <input onChange={this.handleInputChange}/>
+                <button onClick ={this.handleButtonClick}>Add ToDo</button>
                 {this.props.todos.map(todo =>
-                    <div>
+                    <div key={todo.text}>
                         {todo.text}
+                    </div>
+                )}
+                {this.props.visibleTodos.map(visible =>
+                    <div key={visible.text}>
+                        {visible.text}
                     </div>
                 )}
             </div>
