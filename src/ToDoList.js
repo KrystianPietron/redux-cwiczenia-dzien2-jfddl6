eed93294ffd6f1c69de217a0ff54f2045df7c0d2
Krystian Pietroń <krystianpietr@gmail.com>
2018-12-04 10:30:59 +0100

    add props addTodo for todolists

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,34 +1,32 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { addToDo } from './Store/ToDos';
 
+const mapDispatchToProps = dispatch => ({
+    addToDo: text => dispatch(addToDo(text))
+})
 class ToDoList extends React.Component {
-   state  = {}
+    state = {}
 
-   handleInputChange = event => {
-       this.state({value: event.target.value})
-   }
-
-handleButtonClick = () => {
-    console.log('want to save todo: ',this.state.value)
-}
-   
-   
+    handleInputChange = event => {
+        this.setState({ value: event.target.value })
+    }
+    handleButtonClick = () => {
+        console.log('want to save todo: ', this.state.value)
+        this.props.addToDo(this.state.value)
+    }
     render() {
         return (
             <div>
-                <input onChange={this.handleInputChange}/>
-                <button onClick ={this.handleButtonClick}>Add ToDo</button>
+                <input onChange={this.handleInputChange} />
+                <button onClick={this.handleButtonClick}>Add ToDo</button>
                 {this.props.todos.map(todo =>
                     <div key={todo.text}>
                         {todo.text}
-                    </div>
-                )}
-                {this.props.visibleTodos.map(visible =>
-                    <div key={visible.text}>
-                        {visible.text}
                     </div>
                 )}
             </div>
         )
     }
 }
-export default ToDoList
+export default connect(null, mapDispatchToProps)(ToDoList)
