4928d6d0c25c1094a78b68c5b16241388f6da328
Krystian Pietroń <krystianpietr@gmail.com>
2018-12-04 12:19:14 +0100

    add method filter to ToDoList.js

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { addToDo, toggleTodo, deleteTodo } from './Store/ToDos';
+import { addToDo, toggleTodo, deleteTodo, visibleTodos } from './Store/ToDos';
 
 
 const mapStateToProps = state => ({
@@ -11,7 +11,8 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = dispatch => ({
     _addToDo: text => dispatch(addToDo(text)),
     _toogleTodo: index => dispatch(toggleTodo(index)),
-    _deleteTodo: index => dispatch(deleteTodo(index))
+    _deleteTodo: index => dispatch(deleteTodo(index)),
+    _filterTodo: text => dispatch(visibleTodos(text))
 })
 
 
@@ -19,11 +20,11 @@ class ToDoList extends React.Component {
     state = {}
 
     handleInputChange = event => {
-        this.setState({ value: event.target.value })
+        this.setState({ valueInput: event.target.value })
     }
     handleButtonClick = () => {
-        console.log('want to save todo: ', this.state.value)
-        this.props._addToDo(this.state.value)
+        console.log('want to save todo: ', this.state.valueInput)
+        this.props._addToDo(this.state.valueInput)
     }
     handleTodoClick = (index) => {
         this.props._toogleTodo(index)
@@ -31,19 +32,66 @@ class ToDoList extends React.Component {
     handleDelete = (index) => {
         this.props._deleteTodo(index)
     }
+    handleInputSearch = (event) => {
+        this.setState({ valueSearch: event.target.value })
+    }
+    handleButtonSearch = () => {
+        this.props._filterTodo(this.state.valueSearch)
+    }
     render() {
         return (
             <div>
-                <input onChange={this.handleInputChange} />
-                <button onClick={this.handleButtonClick}>Add ToDo</button>
-                {this.props._todos.map((todo, index) =>
-                    <div key={todo.text}
-                        style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
-                    >
-                        <div onClick={() => this.handleTodoClick(index)}>{todo.text}</div>
-                        <div><button onClick={() => this.handleDelete(index)}>X</button></div>
-                    </div>
-                )}
+                {this.renderInput()}
+                {this.renderList()}
+                {this.renderSearch()}
+            </div>
+        )
+    }
+    renderInput() {
+        return (
+            <div>
+                <input
+                    onChange={this.handleInputChange}
+                />
+                <button
+                    onClick={this.handleButtonClick}
+                >
+                    Add ToDo
+            </button>
+            </div>
+        )
+    }
+    renderList() {
+        return this.props._todos.map((todo, index) =>
+            <div key={todo.text}
+                style={{
+                    textDecoration: todo.completed ?
+                        'line-through'
+                        :
+                        'none'
+                }}
+            >
+                <div
+                    onClick={() =>
+                        this.handleTodoClick(index)}
+                >
+                    {todo.text}
+                </div>
+                <div><button
+                    onClick={() =>
+                        this.handleDelete(index)}
+                >
+                    X
+                    </button>
+                </div>
+            </div>
+        )
+    }
+    renderSearch() {
+        return (
+            <div>
+                <input onChange={this.handleInputSearch} />
+                <button onClick={this.handleButtonSearch}>Search</button>
             </div>
         )
     }
